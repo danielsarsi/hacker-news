99b1486495fab36afbb4a0a7f03caf2bb352f129
Daniel Sarsi Orta <daniel@sarsi.cc>
2021-05-31 00:27:26 -0300

    páginas de erro

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+function Erro404() {
+  return (
+    <main>
+      <h1>404</h1>
+    </main>
+  );
+}
+
+export default Erro404;
---
+++ pages/500.tsx
@@ -0,0 +1,9 @@
+function Erro500() {
+  return (
+    <main>
+      <h1>500</h1>
+    </main>
+  );
+}
+
+export default Erro500;
