9765ee478fb6061fbda2315d43b8323f38733f40
Daniel Sarsi Orta <daniel@sarsi.cc>
2021-09-20 13:30:18 +0000

    adiciona headers de segurança

--- next.config.js
+++ next.config.js
@@ -1,16 +1,51 @@
-module.exports = {
+const { PHASE_PRODUCTION_SERVER } = require("next/constants");
+
+const securityHeaders = [
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Content-Security-Policy",
+    value:
+      "default-src 'self' sarsi.cc *.sarsi.cc; connect-src api.hnpwa.com vitals.vercel-insights.com",
+  },
+];
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = (phase) => ({
   redirects() {
     return [
       {
-        source: '/',
-        destination: '/news/1',
+        source: "/",
+        destination: "/news/1",
         permanent: false,
       },
       {
-        source: '/:topico(news|newest|ask|show|jobs)',
-        destination: '/:topico/1',
+        source: "/:topic(news|newest|ask|show|jobs)",
+        destination: "/:topic/1",
         permanent: false,
       },
-    ]
+    ];
   },
-}
+  async headers() {
+    return [
+      phase === PHASE_PRODUCTION_SERVER && {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ].filter(Boolean);
+  },
+});
+
+module.exports = nextConfig;
