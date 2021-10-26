72fd23c9a1c494ba119407f282f512fe16719cd4
Daniel Sarsi Orta <daniel@sarsi.cc>
2021-10-26 21:06:30 +0000

    remove headers de segurança

--- next.config.js
+++ next.config.js
@@ -1,25 +1,7 @@
-const { PHASE_PRODUCTION_BUILD } = require("next/constants");
-
-const securityHeaders = [
-  {
-    key: "X-XSS-Protection",
-    value: "1; mode=block",
-  },
-  {
-    key: "X-Frame-Options",
-    value: "SAMEORIGIN",
-  },
-  {
-    key: "Content-Security-Policy",
-    value:
-      "default-src 'self' 'unsafe-inline'; connect-src 'self' api.hnpwa.com vitals.vercel-insights.com",
-  },
-];
-
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = (phase) => ({
+const nextConfig = () => ({
   redirects() {
     return [
       {
@@ -33,14 +15,6 @@ const nextConfig = (phase) => ({
         permanent: false,
       },
     ];
-  },
-  async headers() {
-    return [
-      phase === PHASE_PRODUCTION_BUILD && {
-        source: "/(.*)",
-        headers: securityHeaders,
-      },
-    ].filter(Boolean);
   },
 });
 
